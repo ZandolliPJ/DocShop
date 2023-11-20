01e0f0b7daffad04d0a1081782a0bf205e1ee122
judith <judithpascal07@gmail.com>
2023-11-20 15:45:51 +0100

    DeleteArticle

--- store/models.py
+++ store/models.py
@@ -24,15 +24,15 @@ class Order(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     ordered = models.BooleanField(default=False)
-
+    ordered_date = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return f"{self.product.name} ({self.quantity})"
 
 class Cart(models.Model):
     user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
     orders = models.ManyToManyField(Order)
-    ordered = models.BooleanField(default=False)
-    ordered_date = models.DateTimeField(blank=True, null=True)
+
+
 
     def __str__(self):
         return self.user.username
